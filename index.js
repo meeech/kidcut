a0c0971e3b07aff8bdbf24a6d835ae2909b1cf05
Mitchell Amihod <mitchell.amihod@gmail.com>
2015-12-10 00:25:58 -0500

    It works

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var cli = require('cli');
 var fs = require('fs');
-var os = require('os');
 var path = require('path');
 var cut = require('./lib/cut.js');
 
@@ -15,14 +14,14 @@ var commands = ['create'];
 cli.parse(options, commands);
 
 cli.main(function(args, options){
-  console.log('cli main', args, options, this.command);
+  // console.log('cli main', args, options, this.command);
 
   var input, output, cut_file, filename;
 
   if(options.input) {
     input = path.resolve(options.input);
     filename = path.basename(input, path.extname(input));
-    console.log('input', filename, input);
+    cli.debug('filename: ' + filename + ' input: ' + input);
   } else {
     cli.fatal('No input file provided.');
   }
@@ -33,7 +32,7 @@ cli.main(function(args, options){
     output = path.dirname(input);
   }
   output = path.normalize(output.concat(path.sep));
-  console.log('output',output);
+  cli.debug('output: ' + output);
 
   if(options.cut) {
     cut_file = path.resolve(options.cut);
