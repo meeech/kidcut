9e958439e2b40bab1a2489eb199f7642ddda5cb8
Mitchell Amihod <mitchell.amihod@gmail.com>
2015-12-12 00:55:33 -0500

    First cut

--- cuts/starwars-ep4.js
+++ cuts/starwars-ep4.js
@@ -12,11 +12,76 @@ module.exports = {
   },
 
   times: [
-    { start: '00:00:00', end: '00:00:02' }
-    , { start: '00:02:00', end: '00:02:02' }
-    , { start: '00:03:00', end: '00:03:02' }
-    , { start: '00:04:00', end: '00:04:02' }
-    , { start: '00:05:00', end: '00:05:02' }
+    { start: '00:00:00', end: '00:03:48' }
+    , { start: '00:03:52', end: '00:03:56' }
+    , { start: '00:04:01', end: '00:04:03' }
+    , { start: '00:04:13', end: '00:04:17' }
+    , { start: '00:04:24', end: '00:05:32' }
+    , { start: '00:06:05', end: '00:06:26' }
+    , { start: '00:06:28', end: '00:07:28' }
+      //vader confronts pricess
+    , { start: '00:07:28', end: '00:08:10' }
+    , { start: '00:08:29', end: '00:13:58' }
+    , { start: '00:14:09', end: '00:14:33' }
+    , { start: '00:14:47', end: '00:18:11' }
+      //right before sandperson scares the shit out of c3p0
+    , { start: '00:18:14', end: '00:28:54' }
+    , { start: '00:29:34', end: '00:34:25' }
+    , { start: '00:34:34', end: '00:36:01' }
+    , { start: '00:36:54', end: '00:37:00' }
+    , { start: '00:38:05', end: '00:38:09' }
+    //skip evil conference of admirals, lack of faith disturbing,
+    //the whole dead jawas, lukes family
+    , { start: '00:40:55', end: '41:00:00' }
+    //cantina
+    , { start: '00:42:28', end: '00:46:35' }
+    , { start: '00:47:19', end: '00:49:58' }
+    //skip greedo :|
+    , { start: '00:51:13', end: '00:51:19' }
+    , { start: '00:52:14', end: '00:52:40' }
+    , { start: '00:54:13', end: '00:57:17' }
+    , { start: '00:59:13', end: '01:02:43' }
+    , { start: '01:03:45', end: '01:11:00' }
+
+    , { start: '01:11:57', end: '01:14:02' }
+    , { start: '01:15:11', end: '01:17:13' }
+    , { start: '01:17:40', end: '01:18:13' }
+    , { start: '01:18:15', end: '01:18:41' }
+    , { start: '01:19:01', end: '01:19:36' }
+    //exit the garbage compactor
+    , { start: '01:25:00', end: '01:25:50' }
+
+    , { start: '01:26:25', end: '01:27:22' }
+    , { start: '01:27:50', end: '01:28:22' }
+    , { start: '01:28:41', end: '01:28:54' }
+    , { start: '01:29:07', end: '01:29:55' }
+    //start obi v dv
+    , { start: '01:29:56', end: '01:31:28' }
+    , { start: '01:31:29', end: '01:31:44' }
+    //lightsaber
+    , { start: '01:31:45', end: '01:31:55' }
+
+    , { start: '01:31:56', end: '01:32:17' }
+
+    //death of obiwan, escape from DS
+    , { start: '01:32:18', end: '01:34:01' }
+
+    // tie fight vs MF
+    , { start: '01:34:02', end: '01:36:29' }
+
+    , { start: '01:38:07', end: '01:38:30' }
+    , { start: '01:38:31', end: '01:41:15' }
+    , { start: '01:41:25', end: '01:41:35' }
+    , { start: '01:43:10', end: '01:45:07' }
+
+    //begin assault on DS
+    , { start: '01:45:22', end: '01:47:19' }
+    , { start: '01:47:38', end: '01:48:11' }
+    , { start: '01:48:13', end: '01:50:40' }
+    , { start: '01:51:42', end: '01:52:40' }
+    , { start: '01:52:54', end: '01:53:20' }
+    , { start: '01:53:34', end: '01:55:05' }
+    , { start: '01:55:14', end: '02:04:39' }
 
   ]
 
