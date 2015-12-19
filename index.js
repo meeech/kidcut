7c40328f63672d5abe4e6ec7f6b295dca3fdc828
Mitchell Amihod <mitchell.amihod@gmail.com>
2015-12-18 22:25:03 -0500

    add a noclean option to leave slices

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var cli = require('cli').enable('status');
 var options = {
   input: ['i', 'Path to the input file', 'file'],
   output_dir: ['o', 'Path to the output directory. Will default to same directory as input', 'dir'],
-  cut: ['c', 'Path to your cut file', 'file']
+  cut: ['c', 'Path to your cut file', 'file'],
+  noclean: ['n', 'Do not clean up temporary clip files. This is useful when working on a clip, since it will speed up your iterations - if we find a previous matching clip, we don\'recreate it' , 'bool']
 };
 
 var commands = ['create', 'list'];
