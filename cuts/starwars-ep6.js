567cc6e8eb2eb8270d6be4995ed62f7c93c7101a
Mitchell Amihod <mitchell.amihod@gmail.com>
2017-04-02 20:52:26 -0400

    Add Return

---
+++ cuts/starwars-ep6.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+  data: {
+    title: 'Star Wars Episode VI Return of the Jedi',
+    description: 'Tried to cut most of the on screen deaths, targeted at 4/5 year old',
+
+    //Can be used to validate you have the same copy?
+    uuid: '',
+
+    //Give your cut a rating
+    rating: ''
+  },
+
+  times: [
+    //vader arrives at new ds
+    { start: '00:00:00', end: '00:04:56' }
+    , { start: '00:04:57', end: '00:10:35' }
+    //leia comes in with chewie
+    , { start: '00:14:26', end: '00:14:56' }
+    , { start: '00:17:26', end: '00:20:23' }
+    , { start: '00:29:35', end: '00:30:10' }
+    , { start: '00:30:17', end: '00:30:38' }
+    , { start: '00:30:50', end: '00:31:25' }
+    , { start: '00:32:00', end: '00:32:54' }
+    //boba gets eaten
+    , { start: '00:33:01', end: '00:33:54' }
+    //cut leia choking jaba - dont want any play reenactments of that
+    , { start: '00:34:18', end: '00:34:24' }
+    , { start: '00:35:02', end: '00:37:05' }
+    //done rescue
+    //death of yoda
+    //dagoba
+    , { start: '00:37:00', end: '00:48:23' }
+    , { start: '00:48:24', end: '00:52:35' }
+    , { start: '00:52:36', end: '00:58:00' }
+    //speeder chase
+    , { start: '00:58:28', end: '01:01:52' }
+    , { start: '01:06:18', end: '01:07:46' }
+    , { start: '01:15:20', end: '01:21:29' }
+    , { start: '01:22:29', end: '01:26:05' }
+    , { start: '01:34:03', end: '01:36:40' }
+    , { start: '01:40:25', end: '01:42:22' }
+    , { start: '01:43:55', end: '02:14:44' }
+  ]
+
+};
